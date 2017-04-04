e888617a8110898abfd765005fdf059a70315984
Andrew Kemp <andrew@ankemp.com>
2017-04-03 20:17:25 -0400

    account.component cleanup

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -3,41 +3,45 @@ import { AngularFire, FirebaseAuthState, FirebaseObjectObservable } from 'angula
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+    selector: 'app-account',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-  item: FirebaseObjectObservable<any>;
-  constructor(public af: AngularFire, private router: Router) { }
+    item: FirebaseObjectObservable<any>;
 
-  ngOnInit() {
-    this.af.auth.subscribe((state: FirebaseAuthState) => {
-      if (state) {
-        console.log('AccountComponent.loggedIn', state);
-        this.item = this.af.database.object(`/members/${state.uid}`);
-        this.item.subscribe(data => {
-          console.log('AccountComponent.item.subscribe', data);
-          this.item.update({
-            name: data.name || state.google.displayName,
-            photo: data.photo || state.google.photoURL
-          });
-          if (!data.name) {
-            this.router.navigate([`/members/${state.uid}`], { queryParams: { edit: true }});
-          }
+    constructor(
+        public af: AngularFire,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.af.auth.subscribe((state: FirebaseAuthState) => {
+            if (state) {
+                console.log('AccountComponent.loggedIn', state);
+                this.item = this.af.database.object(`/members/${state.uid}`);
+                this.item.subscribe(data => {
+                    console.log('AccountComponent.item.subscribe', data);
+                    this.item.update({
+                        name: data.name || state.google.displayName,
+                        photo: data.photo || state.google.photoURL
+                    });
+                    if (!data.name) {
+                        this.router.navigate([`/members/${state.uid}`], { queryParams: { edit: true } });
+                    }
+                });
+            } else {
+                console.log('AccountComponent.notLoggedIn', state);
+            }
         });
-      } else {
-        console.log('AccountComponent.notLoggedIn', state);
-      }
-    });
-  }
+    }
 
-  login() {
-    this.af.auth.login();
-  }
+    login(): void {
+        this.af.auth.login();
+    }
 
-  logout() {
-    this.af.auth.logout();
-  }
+    logout(): void {
+        this.af.auth.logout();
+    }
 
 }
