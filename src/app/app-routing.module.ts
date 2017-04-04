b4a3fee87c2d31698859c708d81511d2f55ec973
Andrew Kemp <andrew@ankemp.com>
2017-04-03 20:04:45 -0400

    Adding new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "app/home/home.component";
 import { LocationsComponent } from "app/locations/locations.component";
+import { LocationComponent } from "app/location/location.component";
 import { MembersComponent } from "app/members/members.component";
 import { MemberComponent } from "app/member/member.component";
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'locations', component: LocationsComponent },
+    { path: 'locations/:id', component: LocationComponent },
     { path: 'members', component: MembersComponent },
     { path: 'members/:id', component: MemberComponent }
 ];
