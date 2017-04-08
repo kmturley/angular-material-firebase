3f1faa281b4594698cb618a5ba321bc47cc61e82
Kim Turley <kim@bynd.com>
2017-04-07 22:31:10 -0400

    Members profile show liked locations

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseAuthState, FirebaseObjectObservable } from 'angularfire2';
+import { AngularFire, FirebaseAuthState, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { Location } from '../locations/locations.component';
 import { Member } from '../members/members.component';
 
 @Component({
@@ -11,6 +12,9 @@ import { Member } from '../members/members.component';
 })
 export class MemberComponent implements OnInit {
     edit: boolean;
+    likes: FirebaseListObservable<any>;
+    locations: FirebaseListObservable<any[]>;
+    locationsLiked: Array<any[]>;
     member: FirebaseObjectObservable<any>;
 
     constructor(
@@ -21,6 +25,15 @@ export class MemberComponent implements OnInit {
     ngOnInit(): void {
         this.route.params.subscribe((params: Params) => {
             this.member = this.af.database.object(`/members/${params['id']}`);
+            this.likes = this.af.database.list(`/likes/${params['id']}`);
+            this.likes.subscribe((likes) => {
+                this.locations = this.af.database.list(`/locations`);
+                this.locations.subscribe((locations) => {
+                    this.locationsLiked = locations.filter((location) => {
+                        return likes.find(x => x.$key === location.$key);
+                    });
+                });
+            });
         });
         this.route.queryParams.subscribe((params: Params) => {
             if (params['edit']) {
