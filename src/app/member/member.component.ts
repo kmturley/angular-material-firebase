1733006e3212fa7170f0e9ef6d6076791fa32140
Kim Turley <kim@bynd.com>
2017-04-11 18:25:27 -0400

    Filter data client side

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 import { Location } from '../locations/locations.component';
 import { Member } from '../members/members.component';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'app-member',
@@ -14,8 +15,7 @@ export class MemberComponent implements OnInit {
     limit = 8;
     edit: boolean;
     likes: FirebaseListObservable<any>;
-    locations: FirebaseListObservable<any[]>;
-    locationsLiked: Array<any[]>;
+    locations: Observable<any[]>;
     member: FirebaseObjectObservable<any>;
     page = 1;
     pageStart = 0;
@@ -29,14 +29,14 @@ export class MemberComponent implements OnInit {
     ngOnInit(): void {
         this.route.params.subscribe((params: Params) => {
             this.member = this.af.database.object(`/members/${params['id']}`);
-            this.locations = this.af.database.list(`/locations`);
             this.likes = this.af.database.list(`/likes/${params['id']}`);
             this.likes.subscribe((likes) => {
-                this.locations.subscribe((locations) => {
-                    this.locationsLiked = locations.filter((location) => {
-                        return likes.find(x => x.$key === location.$key);
+                this.locations = this.af.database.list(`/locations`)
+                    .map((locations) => {
+                        return locations.filter((location) => {
+                            return likes.find(x => x.$key === location.$key) ? true : false;
+                        });
                     });
-                });
             });
         });
         this.route.queryParams.subscribe((params: Params) => {
